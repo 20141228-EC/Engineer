b66cd952cd45dc0bdac89d39a3b12925305ded36
sllllr <2997708711@qq.com>
2025-11-15 22:10:32 +0800

    [feature][FS-251115]<电控-喵板-更新> 回退了系统层的更改

--- Applications/05_System/Include/Core.hpp
+++ Applications/05_System/Include/Core.hpp
@@ -92,15 +92,6 @@ private:
     // 软件复位
     void RESET_SYSTEM();
 
-    // 指针合法性检查
-    EAppStatus Pointer_Check(void *ptr);
-
-    // 模块可用性检查
-    bool HasChassis() const noexcept;
-    bool HasGimbal() const noexcept;
-    bool HasArm() const noexcept;
-    bool HasSubGantry() const noexcept;
-
     // 声明自动操作的任务函数
     static void StartReturnOriginTask(void *arg);
 
