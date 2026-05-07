d28361322694de6b0ba264b6f8c26b1870ac452a
sllllr <2997708711@qq.com>
2026-05-07 16:48:46 +0800

    修复x y轴速度错误问题

--- Applications/05_System/Core/Core.cpp
+++ Applications/05_System/Core/Core.cpp
@@ -522,9 +522,8 @@ void CSystemCore::Chassis_UpdateHandler_(){
         else{
             cycle = std::clamp(yaw_angle * 4, -100.f, 100.f);    // 50是magic number,后续需要调整
         }   // 开小陀螺与否
-        // chassisCmd_.speed_y_ = front * cos(yaw_angle) - right * sin(yaw_angle);
-        chassisCmd_.speed_y_ = -(right * cos(yaw_angle) + front * sin(yaw_angle));
-        chassisCmd_.speed_x_ = (front * cos(yaw_angle) - right * sin(yaw_angle));   // 根据云台角度计算底盘运动正方向
+        chassisCmd_.speed_y_ = front * cos(yaw_angle) - right * sin(yaw_angle);
+        chassisCmd_.speed_x_ = right * cos(yaw_angle) + front * sin(yaw_angle); // 根据云台角度计算底盘运动正方向
         chassisCmd_.speed_w_ = cycle;
     }
     else{
