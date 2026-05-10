2cdd8d4347c4a052ca3451b1592b412249ef73e2
sllllr <2997708711@qq.com>
2026-05-11 05:11:06 +0800

    板通更新,推送漏掉的系统层.hpp文件

--- Applications/05_System/Include/sys_board_link.hpp
+++ Applications/05_System/Include/sys_board_link.hpp
@@ -69,6 +69,7 @@ public:
         uint8_t is_spin_on;     ///< 是否开小陀螺
         uint8_t autoTask;   /// 自动任务编号
         uint8_t use_controller; ///< 是否使用控制器
+        uint8_t ResetFlag;  ///< 软件复位
      }__packed otherInfos = {};
 
     /**
